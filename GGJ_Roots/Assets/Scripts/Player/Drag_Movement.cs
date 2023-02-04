e0529d622403fc724f887a18c89fca5ee9fb5752
ivu-u <113319454+ivu-u@users.noreply.github.com>
2023-02-04 17:29:41 -0500

    scene fader add
    has not been tested

--- GGJ_Roots/Assets/Scripts/Player/Drag_Movement.cs
+++ GGJ_Roots/Assets/Scripts/Player/Drag_Movement.cs
@@ -29,7 +29,7 @@ public class Drag_Movement : MonoBehaviour
     private void Update()
     {
         // check if player is moving
-        if(rb.velocity == new Vector2(0,0))     // if time adjust this
+        if(rb.velocity.magnitude <= 0.5)     // if time adjust this
         {
             isStill = true;
         }
