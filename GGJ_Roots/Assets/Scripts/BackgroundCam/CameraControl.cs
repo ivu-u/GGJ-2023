299da6be6e2e897fcb56c051e4451f69ba3ab53b
ivu-u <113319454+ivu-u@users.noreply.github.com>
2023-02-04 16:58:39 -0500

    vertical infinite scrolling

---
+++ GGJ_Roots/Assets/Scripts/BackgroundCam/CameraControl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform bckg1;
+    public Transform bckg2;
+
+    private float size;
+
+    private void Start()
+    {
+        size = bckg1.GetComponent<BoxCollider2D>().size.y;
+    }
+
+    void FixedUpdate()
+    {
+        // camera
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = targetPos;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
+
+        // background
+        if(transform.position.y >= bckg2.position.y)
+        {
+            bckg1.position = new Vector3(bckg1.position.x, bckg2.position.y + size, bckg1.position.z);
+            SwitchBckg();
+        }
+
+        if(transform.position.y < bckg1.position.y) 
+        { 
+            bckg2.position = new Vector3(bckg2.position.x, bckg1.position.y - size, bckg2.position.z);
+            SwitchBckg();
+        }
+    }
+
+    private void SwitchBckg()
+    {
+        Transform temp = bckg1;
+        bckg1 = bckg2;
+        bckg2 = temp;
+    }
+}
